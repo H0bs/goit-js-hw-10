ba94a01446f28024dc5080846543f0ae9f945e5a
Oleksandr Veremchuk <verem4uk@hotmail.com>
2022-09-02 22:10:03 +0300

    fix

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,6 @@
 export default function fetchCountries(name) {
     const input = name.target.value.trim();
-    console.log(input);
+    if (input) {
     return fetch(`https://restcountries.com/v2/name/${input}?fields=name,capital,population,flags,languages`)
         .then(res => {
             if (!res.ok) {
@@ -8,4 +8,5 @@ export default function fetchCountries(name) {
             }
             return res.json();
         } );
+    }
 }
